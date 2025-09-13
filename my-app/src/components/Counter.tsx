23977714102d948a8f5231c4e61a26abd5fa89b7
KL1 <karl@luberg.ee>
2025-09-13 16:48:41 +0300

    feat(counter): added increase and decrease buttons to counter
    added buttons to adjust the counter value by +1, +25, +50, +100
    and -1, -25, -50, -100

--- my-app/src/components/Counter.tsx
+++ my-app/src/components/Counter.tsx
@@ -12,8 +12,32 @@ function Counter() {
     <>
       <h1>Vite + React</h1>
       <div className="card">
+        <button onClick={() => increaseCounter(-100)}>
+          -100
+        </button>
+        <button onClick={() => increaseCounter(-50)}>
+          -50
+        </button>
+        <button onClick={() => increaseCounter(-25)}>
+          -25
+        </button>
+        <button onClick={() => increaseCounter(-1)}>
+          -1
+        </button>
         <button onClick={() => increaseCounter(1)}>
           count is {count}
+        </button>
+        <button onClick={() => increaseCounter(1)}>
+          +1
+        </button>
+        <button onClick={() => increaseCounter(25)}>
+          +25
+        </button>
+        <button onClick={() => increaseCounter(50)}>
+          +50
+        </button>
+        <button onClick={() => increaseCounter(100)}>
+          +100
         </button>
       </div>
     </>
